af8dfade990c99c3e30b4981a46ba4d1a743f309
aj-ay2706 <118709615+aj-ay2706@users.noreply.github.com>
2022-11-26 09:03:48 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './index.css';
+import RegisterForm from "./form";
+import { FormControl } from '@material-ui/core';
+import "./App.css";
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm/>
+    </div>
+      
+
+  );
+}
+
+export default App;
